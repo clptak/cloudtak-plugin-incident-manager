f826e511235a4e3505d651967d971e0c9cf6f426
clptak <paul.clifton@clptak.com>
2026-09-16 07:43:48 -0700

    Add LpbTaskbarChip and LPB button to IncidentManager and Popout components
    - Integrated LpbTaskbarChip into IncidentManagerChipBar for enhanced task management.
    - Added a button in PopoutRoot to open the LPB distance table, improving accessibility to LPB features.
    - Updated useIncident composable to include isSearchMission for better state management.

---
+++ src/components/LpbTaskbarChip.vue
@@ -0,0 +1,23 @@
+<template>
+    <TaskbarChipButton
+        v-if='isSearchMission'
+        :icon='IconRuler2'
+        label='LPB'
+        title='Open LPB distance table'
+        @click='openLpb'
+    />
+</template>
+
+<script setup lang='ts'>
+import { IconRuler2 } from '@tabler/icons-vue';
+import TaskbarChipButton from './TaskbarChipButton.vue';
+import { openDesktopPane } from '../lib/floatMinimize.ts';
+import { useIncident } from '../composables/useIncident.ts';
+
+const { selectHTabGuarded, isSearchMission } = useIncident();
+
+function openLpb(): void {
+    openDesktopPane();
+    selectHTabGuarded('lpb');
+}
+</script>
